cc88afe4f7ec920f3c242b3e9c9d4eb6073f227c
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-01-21 23:52:35 -0500

    wip(): Nacimientos routes

--- routes/nacimientos/index.js
+++ routes/nacimientos/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../models');
-router.get('/', async (req, res) =>{
+router.get('/nacidos', async (req, res) =>{
     let nacidos = await db.nacidos.findAll();
     try{
         res.send({data : nacidos});
+    } catch(err){
+        res.send({message : err});
+    }
+})
+
+router.get('/padres', async (req, res) =>{
+    let padres = await db.padres.findAll();
+    try{
+        res.send({data : padres});
+    } catch(err){
+        res.send({message : err});
+    }
+})
+router.get('/madres', async (req, res) =>{
+    let madres = await db.madres.findAll();
+    try{
+        res.send({data : madres});
     } catch(err){
         res.send({message : err});
     }
